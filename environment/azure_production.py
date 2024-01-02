aa61aa8265140babec57381835794b25cf008c7c
JEEVAN S <chuppijeevan@gmail.com>
2024-01-02 18:51:54 +0530

    Update azure_production.py

--- environment/azure_production.py
+++ environment/azure_production.py
@@ -1,11 +1,18 @@
 import os
+from urllib.parse import urlparse
 
-conn_str = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# Your new connection string
+conn_str = "postgres://findartist_user:7XOrSKhLhYl7P4Y7uctMlkU8qEfF7Lix@dpg-cma0q8md3nmc73b8undg-a.singapore-postgres.render.com/findartist"
 
-DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
-)
+# Parse the connection string
+parsed_url = urlparse(conn_str)
+
+# Extract the components from the parsed URL
+DB_USER = parsed_url.username
+DB_PASSWORD = parsed_url.password
+DB_HOST = parsed_url.hostname
+DB_PORT = parsed_url.port
+DB_NAME = parsed_url.path.lstrip('/')
+
+# Create the new DATABASE_URI
+DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
