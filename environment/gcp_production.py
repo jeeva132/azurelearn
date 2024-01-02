8c96871d7e2d37dd958ed883f9b80b799716cb1a
JEEVAN S <chuppijeevan@gmail.com>
2024-01-02 18:51:35 +0530

    Update gcp_production.py

--- environment/gcp_production.py
+++ environment/gcp_production.py
@@ -1,9 +1,17 @@
 import os
 
-DATABASE_URI = "{db_connector}://{db_user_name}:{db_password}@/{db_name}?unix_sock={unix_socket_path}/.s.PGSQL.{db_port}".format(
-    db_connector=os.environ.get('DB_CONNECTOR'),
-    db_user_name=os.environ.get('DB_USER_NAME'),
-    db_password=os.environ.get('DB_PASSWORD'),
-    db_name=os.environ.get('DB_NAME'),
-    unix_socket_path=os.environ.get('INSTANCE_UNIX_SOCKET'),
-    db_port=os.environ.get('DB_PORT'))
+# Your new connection string
+new_conn_str = "postgresql://findartist_user:7XOrSKhLhYl7P4Y7uctMlkU8qEfF7Lix@dpg-cma0q8md3nmc73b8undg-a.singapore-postgres.render.com/findartist"
+
+# Parse the new connection string
+parsed_url = urlparse(new_conn_str)
+
+# Extract the components from the parsed URL
+DB_CONNECTOR = parsed_url.scheme
+DB_USER_NAME = parsed_url.username
+DB_PASSWORD = parsed_url.password
+DB_NAME = parsed_url.path.lstrip('/')
+DB_PORT = parsed_url.port
+
+# Create the new DATABASE_URI
+DATABASE_URI = f"{DB_CONNECTOR}://{DB_USER_NAME}:{DB_PASSWORD}@/{DB_NAME}?unix_sock=/{DB_PORT}"
